936115e1897004dbc2d097b0376d909e2deec409
unknown <macoycoder@gmail.com>
2022-07-30 23:01:48 +0800

    Initialized express and sequalize

---
+++ server/app/models/index.js
@@ -0,0 +1,21 @@
+// Setting up Sequalize
+
+const dbConfig = require("../config/db.config");
+const Sequalize = require("sequelize");
+const sequelize = new Sequalize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+	host: dbConfig.host,
+	dialect: dbConfig.dialect,
+	operatorAliases: false,
+	pool: {
+		max: dbConfig.pool.max,
+		min: dbConfig.pool.min,
+		acquire: dbConfig.pool.acquire,
+		idle: dbConfig.pool.idle,
+	},
+});
+
+const db = {};
+db.Sequalize = Sequelize;
+db.sequalize = sequalize;
+
+// models or tables
---
+++ server/server.js
@@ -0,0 +1,22 @@
+// Setting up the server
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const corsOptions = {
+	origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.json({ message: "Server testing." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
