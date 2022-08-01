985d3aceb33b5dc6c166028c0f4158ab64a03b68
unknown <macoycoder@gmail.com>
2022-08-01 15:37:40 +0800

    Added CRUD in inventory controller

---
+++ server/app/controller/inventory.controller.js
@@ -0,0 +1,86 @@
+// This module contains the controller methods for inventory
+
+// utils
+const Error_Message = require("../utils/err_message.utils");
+
+const db = require("../models");
+const Inventory = db.inventory;
+
+// Reading all the products in the inventory
+exports.readInventory = (req, res) => {
+	Inventory.findAll()
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			Error_Message(
+				res,
+				500,
+				err.message || "Some error occurred while reading the inventory."
+			);
+		});
+};
+
+// Creating a new Product in the inventory
+exports.createInventory = (req, res) => {
+	const product = {
+		quantity: req.body.quantity,
+		productID: req.body.productID,
+	};
+
+	Inventory.create(product)
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			Error_Message(
+				res,
+				500,
+				err.message || "Some error occured while creating a product."
+			);
+		});
+};
+
+// Update a Product in Inventory
+exports.updateInventory = (req, res) => {
+	const id = req.params.id;
+
+	Inventory.update(req.body, {
+		where: { id: id },
+	})
+		.then((row) => {
+			if (row == 1) {
+				res.send({ message: 1 });
+			} else {
+				res.send({ message: 0 });
+			}
+		})
+		.catch((err) => {
+			Error_Message(
+				res,
+				500,
+				err.message || "Some error occured while updating inventory."
+			);
+		});
+};
+
+// Delete a Product in inventory
+exports.deleteProduct = (req, res) => {
+	const id = req.params.id;
+
+	Inventory.destroy({ where: { id: id } })
+		.then((row) => {
+			if (row == 1) {
+				res.send({ message: 1 });
+			} else {
+				res.send({ message: 0 });
+			}
+		})
+		.catch((err) => {
+			Error_Message(
+				res,
+				500,
+				err.messsage || "Some error occured while deleting product."
+			);
+		});
+};
