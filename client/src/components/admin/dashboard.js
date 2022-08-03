dd61f672a3316deafbe37d5456dbc25162503df8
unknown <macoycoder@gmail.com>
2022-08-03 22:02:12 +0800

    Added links and dashboard style

--- client/src/components/admin/dashboard.js
+++ client/src/components/admin/dashboard.js
@@ -1,10 +1,43 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 import React from "react";
 
 function Dashboard() {
 	return (
-		<div className="container mt-3">
+		<div className="container text-center mt-5">
 			<h1>Admin Dashboard</h1>
+			<div className="row gy-3 mt-3">
+				<Link to={"/admin/users"}>
+					<div className="col-sm-12 col-md-6 col-lg-4">
+						<button
+							type="button"
+							className="btn btn-outline-primary btn-lg p-4 w-100"
+						>
+							Users
+						</button>
+					</div>
+				</Link>
+				<Link to={"/admin/products"}>
+					<div className="col-sm-12 col-md-6 col-lg-4">
+						<button
+							type="button"
+							className="btn btn-outline-primary btn-lg p-4 w-100"
+						>
+							Products
+						</button>
+					</div>
+				</Link>
+				<Link to={"/admin/inventory"}>
+					<div className="col-sm-12 col-md-6 col-lg-4">
+						<button
+							type="button"
+							className="btn btn-outline-primary btn-lg p-4 w-100"
+						>
+							Inventory
+						</button>
+					</div>
+				</Link>
+			</div>
 		</div>
 	);
 }
