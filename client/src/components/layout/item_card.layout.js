f844689bec20578ada3b488942ccac8b8ae890d3
unknown <macoycoder@gmail.com>
2022-08-17 22:58:40 +0800

    Created a reusable component for item cards

---
+++ client/src/components/layout/item_card.layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ItemCard = (props) => {
+	return (
+		<div className="col-12 col-md-6 col-lg-4">
+			<div className="card">
+				<div className="card-body">
+					<h2 className="card-title">Logitech Mouse</h2>
+					<h4 className="card-subtitle mb-2">&#8369; 5,000.00</h4>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+						voluptate ex, veniam reiciendis aperiam, sunt natus qui, tempora
+						saepe dolorum labore expedita autem porro fugit omnis laborum
+						corrupti unde. Pariatur.
+					</p>
+					<button className="d-block w-100 btn btn-primary mx-auto">
+						Learn More
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ItemCard;
--- client/src/components/public/products.js
+++ client/src/components/public/products.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Link, userNavigate } from "react-router-dom";
 
 // components
 import ProductService from "../../services/ProductService";
+import ItemCard from "../layout/item_card.layout";
+import Navigation from "../layout/navigation.layout";
 
 const Product = () => {
 	useEffect(() => {
@@ -19,7 +22,21 @@ const Product = () => {
 	};
 	return (
 		<div>
-			<h1>Product</h1>
+			<Navigation />
+			<div className="container-fluid mt-3">
+				<h1 className="text-center">Product</h1>
+				<div className="col-12 d-flex flex-row flex-wrap gap-2">
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+					<ItemCard />
+				</div>
+			</div>
 		</div>
 	);
 };
