cf079cc25c8a0fe99dabbe0e845e8cba0e1e73bf
unknown <macoycoder@gmail.com>
2022-08-18 13:31:15 +0800

    Added pictures for product cards

--- client/src/components/layout/item_card.layout.js
+++ client/src/components/layout/item_card.layout.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ProductPicture from "../../assets/product.jpg";
 
 const ItemCard = (props) => {
 	return (
 		<div className="col-12 col-md-6 col-lg-4 mb-3 mb-lg-4">
 			<div className="card">
+				<img src={ProductPicture} alt="Product" className="card-img-top" />
 				<div className="card-body">
 					<h2 className="card-title">Logitech Mouse</h2>
 					<h4 className="card-subtitle mb-2">&#8369; 5,000.00</h4>
