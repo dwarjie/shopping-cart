944386386933d1c0374c122b240fb69ab46824a1
unknown <macoycoder@gmail.com>
2022-08-20 21:10:42 +0800

    Product item viewing now working properly. But can't add to cart yet

--- client/src/components/public/product_item.js
+++ client/src/components/public/product_item.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 // component
+import ProductPicture from "../../assets/product.jpg";
 import ProductService from "../../services/ProductService";
 import Navigation from "../layout/navigation.layout";
 
 const ProductItem = () => {
 	const { id } = useParams();
 	const [product, setProduct] = useState([]);
+	const [quantity, setQuantity] = useState(1);
 
 	useEffect(() => {
 		getItem(id);
-	}, []);
+	}, [id]);
 
 	const getItem = (id) => {
 		ProductService.getItem(id)
@@ -24,9 +26,40 @@ const ProductItem = () => {
 			});
 	};
 
+	const handleInputChange = (event) => {
+		const quantity = event.target.value;
+		setQuantity(quantity);
+	};
+
 	return (
 		<div>
 			<Navigation />
+			<div className="container-fluid mt-3 d-flex flex-column flex-lg-row gap-5">
+				<div className="col col-lg-6 d-block mx-auto">
+					<img
+						src={ProductPicture}
+						alt="Product"
+						className="img-fluid img-thumbnail mb-1 mb-lg-0"
+					/>
+				</div>
+				<div className="col col-lg-6">
+					<h1>
+						<strong>{product.name}</strong>
+					</h1>
+					<h3 className="mb-3">&#8369; {product.price}</h3>
+					<p className="lead">{product.desc}</p>
+					<label htmlFor="quantity">Quantity:</label>
+					<input
+						type="number"
+						name="quantity"
+						id="quantity"
+						className="form-control w-25 mb-3"
+						value={quantity}
+						onChange={handleInputChange}
+					/>
+					<button className="btn btn-primary w-100">Add to Cart</button>
+				</div>
+			</div>
 		</div>
 	);
 };
