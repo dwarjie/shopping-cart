36935224abce0c225818c8b1365b9886099ed2d5
unknown <macoycoder@gmail.com>
2022-08-23 22:44:55 +0800

    Cart items component done

--- client/src/components/layout/cart_item.layout.js
+++ client/src/components/layout/cart_item.layout.js
@@ -1,31 +1,32 @@
 // this reusable component will be used for displaying the items of user in their shopping cart
 import React, { useState } from "react";
+import { TrashFill } from "react-bootstrap-icons";
+
+// components
 import ProductPicture from "../../assets/product.jpg";
 
 const CartItem = (props) => {
 	return (
-		<div className="col-12 d-flex flex-row justify-content-between align-content-start">
-			<div className="d-flex flex-row align-content-start col-8">
-				<img src={ProductPicture} alt="Product" className="img-fluid col-5" />
+		<div className="col-12 d-flex flex-row justify-content-between align-content-start p-2">
+			<div className="d-flex flex-row align-content-start col-10 gap-4">
+				<img
+					src={ProductPicture}
+					alt="Product"
+					className="img-fluid col-6 col-md-5 col-lg-3"
+				/>
 				<div>
-					<h3>
+					<h3 className="fs-6">
 						<strong>{props.data.product.name}</strong>
 					</h3>
-					<h3>&#8369; 500.00</h3>
+					<h3 className="fs-6">&#8369; 500.00</h3>
 				</div>
 			</div>
-			<div className="col-4">
+			<div className="col d-flex flex-column justify-content-between align-content-end">
+				<TrashFill className="text-danger fs-6 c-pointer d-block mx-auto" />
 				<div className="d-flex flex-row align-content-start justify-content-center">
-					<span>-</span>
-					<input
-						type="text"
-						name="quantity"
-						id="quantity"
-						className="border-0"
-						readOnly={true}
-						value={props.data.quantity}
-					/>
-					<span>+</span>
+					<span className="mx-4 c-pointer">-</span>
+					<p>{props.data.quantity}</p>
+					<span className="mx-4 c-pointer">+</span>
 				</div>
 			</div>
 		</div>
